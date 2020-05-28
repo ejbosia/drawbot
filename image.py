3bf9e6eb8a136eb76f2a502ea4565fa09ea07e3b
Evan Bosia <evanjbosia@localhost.localdomain>
2020-05-28 19:57:04 -0400

    Adding raster image support

---
+++ image.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+# TODO: test using np.where
+
+# returns a list of "points" for each position
+# every point travels up, to the point, and then down
+def raster_image(image):
+
+    pos_list = []
+
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            if(image[row,col] > 0):
+
+                pos_list.append({"Z": 10)
+                pos_list.append({
+                                    "X": col,
+                                    "Y": row
+                                    })
+                pos_list.append({"Z": 0})
+
+    return pos_list
