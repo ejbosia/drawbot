dd35d72b37a58f50e226385f2eb690027b6d6554
Evan Bosia <evanjbosia@localhost.localdomain>
2020-05-28 19:44:06 -0400

    Adding gcode function

--- gcode_test.py
+++ gcode_test.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+# takes in a position (XYZF)
+# returns the gcode to enact that position
+def pos_gcode(pos):
+
+    gcode = "\bG01 "
+
+    for key in pos.keys():
+        gcode += key+str(pos[key])+" "
+
+    gcode += ";"
+
+    return gcode
+
+
+
 def main():
     print("This is a test")
 
