51e45d6ed812e3f817924f4ca7604ce7e22007ac
Evan Bosia <evanjbosia@localhost.localdomain>
2020-05-31 14:45:39 -0400

    Adding gcode optimizer
    Still working on the exact algorithm, but it will look for straight lines to combine into single gcode statements

--- gcode.py
+++ gcode.py
@@ -24,6 +24,75 @@ def pos_list_gcode(pos_list):
 
     return gcode
 
+# get the next avaiable partition
+# start looking to the right of the partition, clockwise search
+def next_paritition(pt, points):
+
+    # array of the different directions around the points
+    # these values are added to the test point to create the check points
+    check = np.array([
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1],
+        [0,-1],
+        [-1,-1],
+        [-1,0],
+        [-1,1]
+    ])
+
+    for c in check:
+        check_pt = pt + c
+
+        mask = ((check_pt == points).all(axis=1))
+
+        if(mask.any()):
+            return points[mask][0]
+
+    return available_pts
+
+
+
+# returns the spiral fill of gcode
+def chain_spiral(chain):
+    gcode = ""
+
+    # sort the partitions (first is top left, sorts row before column)
+    sort_chain = np.array(chain).sort(axis=0)
+
+    # take the first partition out of the list
+    pt = sort_chain[0]
+    sort_chain = np.delete(sort_chain, 0, axis=0)
+
+    check = np.array([
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1],
+        [0,-1],
+        [-1,-1],
+        [-1,0],
+        [-1,1]
+    ])
+
+    direction = 0
+
+    while sort_chain.size > 0:
+
+        check_pt = pt + check[direction]
+
+        mask = ((check_pt == points).all(axis=1))
+        # if a point exists in the direction
+        if(mask.any()):
+            pt = check_pt
+            sort_chain = sort_chain[(pt != sort_chain).any(axis=1)]
+        # if the search direction is past
+        elif direction > 7:
+            pt = sort_chain[0]
+            sort_chain = np.delete(sort_chain, 0, axis=0)
+        else:
+            pos_gcode(pt)
+    return gcode
 
 
 def main():
