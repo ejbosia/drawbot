f0d6932f8504709292ad99a9189a4fe904c925a2
ejbosia <ejbosia@gmail.com>
2021-04-18 17:54:24 -0400

    Adding zig zag and outline fill
    Theses are strategies for taking a list of polygons and outputting a path

--- python/shade_strategy/gradient_fill.py
+++ python/shade_strategy/gradient_fill.py
@@ -9,9 +9,4 @@ Generate an easy gradient fill of the image
 # generate the gradient fill of the image
 def execute(image, skip_row=5):
 
-    # iterate through the image from the top 
-    for i in range(0, len(image), skip_row):
-
-        print(i)
-        
-    print("NOT DONE")
+    raise NotImplementedError
